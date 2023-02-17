5faf7e21342c42ae639f8d3a19b6ce65250e7614
Efoneli <abbaefoneli@gmail.com>
2023-02-17 15:29:01 +0100

    created main

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+       <div>
+            <nav>
+                <ul>
+                    <li><a href='#'>Popular</a></li>
+                    <li><a href='#'>Theater</a></li>
+                    <li><a href='#'>Kids</a></li>
+                    <li><a href='#'>Drama</a></li>
+                    <li><a href='#'>Comedy</a></li>
+
+                </ul>
+            </nav>
+        </div> 
+    </>
+  )
+}
+
+export default Main
