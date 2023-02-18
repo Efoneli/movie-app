b5c5c5b223fe4413f835dcd140fc0f266e008c19
Efoneli <abbaefoneli@gmail.com>
2023-02-18 15:40:07 +0100

    adding logo

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,9 +10,11 @@ const Card = (movie) => {
             <img src={img_path+movie.info.poster_path} className='poster' />
             <div className='movie-details'>
               <div className="box">
-                <h4 className="title">{movie.info.title}</h4>
+                <h4 className="title font-bold">{movie.info.title}</h4>
                 <p className='rating'>{movie.info.vote_average}</p>
               </div>
+              <h6 className='mr-6 italic'>Released: {movie.info.release_date}</h6>
+
               <div className="overview">
                 <h1 className='font-bold text-2xl'>Overview</h1>
                 {movie.info.overview}
